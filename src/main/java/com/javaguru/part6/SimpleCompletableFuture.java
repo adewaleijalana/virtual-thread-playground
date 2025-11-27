cf6560e7abc6db1653b86703d20796aba14bba5d
adewaleijalana <adewaleijalana@gmail.com>
2025-11-27 11:53:32 +0100

    simple completablefuture task with virtual thread

--- src/main/java/com/javaguru/part6/SimpleCompletableFuture.java
+++ src/main/java/com/javaguru/part6/SimpleCompletableFuture.java
@@ -1,8 +1,10 @@
 package com.javaguru.part6;
 
+import com.javaguru.util.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
 public class SimpleCompletableFuture {
@@ -10,9 +12,11 @@ public class SimpleCompletableFuture {
 
     static void main() {
         log.info("Starting SimpleCompletableFuture in main");
-        var result = fastTask();
-        log.info("result: {}", result.join());
+        var result = slowTask();
+        result.thenAccept(s ->  log.info("result: {}",s));
         log.info("Finished SimpleCompletableFuture in main");
+
+        CommonUtils.sleep(Duration.ofSeconds(3));
     }
 
     private static CompletableFuture<String> fastTask() {
@@ -20,6 +24,18 @@ public class SimpleCompletableFuture {
         var completableFuture = new CompletableFuture<String>();
         completableFuture.complete("Hello World of CompletableFuture");
         log.info("Finished SimpleCompletableFuture fastTask");
+        return completableFuture;
+    }
+
+
+    private static CompletableFuture<String> slowTask() {
+        log.info("Starting SimpleCompletableFuture slowTask");
+        var completableFuture = new CompletableFuture<String>();
+        Thread.ofVirtual().start(() -> {
+            CommonUtils.sleep(Duration.ofSeconds(2));
+            completableFuture.complete("Hello World of CompletableFuture");
+        });
+        log.info("Finished SimpleCompletableFuture slowTask");
         return completableFuture;
     }
 }
